8299ab98af4cd3d9d922f615c31b285ea2bb9742
fjavierduarte <57017570+fjavierduarte@users.noreply.github.com>
2022-03-06 21:23:55 -0300

    Hasta menu y scrollbar

---
+++ components/Header.jsx
@@ -0,0 +1,68 @@
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import { styled } from "@mui/system";
+
+import IconButton from "@mui/material/IconButton"
+import Home from "@mui/icons-material/Home"
+//@components es un compilerOptions agegado en jsconfig.json
+import MuiNextLink from "@components/MuiNextLink";
+import Navbar from "@components/Navbar";
+import SideDrawer from "@components/SideDrawer";
+import HideOnScroll from "@components/HideOnScroll";
+
+import Fab from "@mui/material/Fab";
+import KeyboardArrowUp from "@mui/icons-material/KeyboardArrowUp";
+import BackToTop from "@components/BackToTop";
+
+
+
+const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
+
+//Son los links para la barra de navegación
+export const navLinks = [
+  { title: `home`, path: `/` },
+  { title: `about us`, path: `/about-us` },
+  { title: `menu`, path: `/menu` },
+  { title: `catering`, path: `/catering` },
+  { title: `contact`, path: `/contact` },
+];
+
+const Header = () => {
+  return (
+    <>
+      <HideOnScroll>      
+        <AppBar position="fixed">
+          <Toolbar>
+            <Container
+              maxWidth="lg"
+              sx={{ display: `flex`, justifyContent: `space-between` }}
+            >
+              <IconButton edge="start" aria-label="home">
+                <MuiNextLink activeClassName="active" href="/">
+                  <Home
+                    sx={{
+                    color: (theme) => theme.palette.common.white,
+                    }}
+                    fontSize="large"
+                  />
+                </MuiNextLink>
+              </IconButton>
+              <Navbar navLinks={navLinks}/>
+              <SideDrawer navLinks={navLinks}/>
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>  
+
+      <Offset id="back-to-top-anchor"/>
+      <BackToTop>
+        <Fab color="secondary" size="large" aria-label="back to top">
+          <KeyboardArrowUp />
+        </Fab>
+      </BackToTop>
+    </>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
@@ -6,6 +7,7 @@ import createEmotionCache from '../styles/createEmotionCache';
 
 import "/styles/globals.css";
 import theme from '../styles/theme';
+import Header from "@components/Header";
 
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -23,6 +25,7 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Header/>
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
