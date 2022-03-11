7c4bfffdb7f433f07bb889057912c88be4be3b0b
fjavierduarte <57017570+fjavierduarte@users.noreply.github.com>
2022-03-11 10:14:58 -0300

    Se incluye la imagen Hero

---
+++ components/Hero.jsx
@@ -0,0 +1,65 @@
+import Grid from "@mui/material/Grid";
+import Image from "next/image";
+import Typography from "@mui/material/Typography";
+
+const Hero = ({imgSrc, imgAlt, title, subtitle}) => {
+  return ( 
+    <Grid
+      component="section"
+      container
+      sx={{
+        position: `relative`,
+        height: "100vh",
+        width: `100vw`,
+        overflow: `hidden`,
+        zIndex: -100,
+        mb: 15,
+      }}
+    >
+      <Image src={imgSrc} alt={imgAlt} layout="fill" objectFit="cover" />  
+      <Grid
+        container
+        sx={{
+          position: "absolute",
+          inset: 0,
+          backgroundColor: "rgba(0,0,0, .7)",
+        }}
+      ></Grid>   
+      <Grid
+        container
+        item
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography
+          variant="h1"
+          align="center"
+          gutterBottom
+          sx={{
+            color: "secondary.main",
+            fontWeight: 400,
+            zIndex: 0,
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          component="p"
+          variant="h3"
+          align="center"
+          color="common.white"
+          sx={{
+            mb: 10,
+            zIndex: 0,
+          }}
+        >
+          {subtitle}
+        </Typography>
+      </Grid>         
+      </Grid>
+
+  );
+}
+
+export default Hero;
